bc181301256404d1398ec69ec6abc369bb495e54
Plaisir <plaisir1997@live.nl>
2017-03-09 14:21:58 +0100

    index a href knoppen

--- index.php
+++ index.php
@@ -29,18 +29,26 @@ if (isset($_SESSION['token'])) {
     </head>
     <body>
     <div class="container">
-        <div class="options option1">
-            <p id="options">Laatst afgespeeld</p>
-        </div>
-        <div class="options option2">
-            <p id="options">Favorieten lijst</p>
-        </div>
-        <div class="options option3">
-            <p id="options">Artiesten</p>
-        </div>
-        <div class="options option4">
-            <p id="options">Nieuwe muziek</p>
-        </div>
+        <a href="laatst-afgespeeld.php">
+            <div class="options option1">
+                <p id="options">Laatst afgespeeld</p>
+            </div>
+        </a>
+        <a href="favorieten.php">
+            <div class="options option2">
+                <p id="options">Favorieten lijst</p>
+            </div>
+        </a>
+        <a href="artiesten.php">
+            <div class="options option3">
+                <p id="options">Artiesten</p>
+            </div>
+        </a>
+        <a href="nieuwe-muziek.php">
+            <div class="options option4">
+                <p id="options">Nieuwe muziek</p>
+            </div>
+        </a>
     </div>
     </body>
     </html>
