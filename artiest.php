1dd3de146f6e18c9c992b7881c52174f1bc9a7e3
Plaisir <plaisir1997@live.nl>
2017-03-16 12:36:39 +0100

    artiestuhhh

--- artiest.php
+++ artiest.php
@@ -28,22 +28,81 @@ if (isset($_GET['id'])) {
     <title>Artiest</title>
 </head>
 <body>
+<div class="container">
+    <a href="index.php">
+        <img class="logo pull-right" src="img/Logo.png" alt="">
+    </a>
+</div>
+<div class="container">
 
-<?php
+    <?php
+
 $tracks = $api->getArtistTopTracks($id, [
     'country' => 'us',
 ]);
+//    print '<pre>';
+//    print_r($tracks);
+//    print '</pre>';
 
-    print "<pre>";
-print_r($tracks);
-    print "</pre>";
+$count = 0;
+$number = 0;
+$array1 = array();
 
-//foreach($tracks)
 ?>
+    <h1 class="bigTitle"><?php echo $tracks->tracks[0]->album->artists[0]->name; ?></h1>
 
+<?php
+foreach(array_chunk($tracks->tracks, 4, true) as $array){
+    $count++;
+    echo "<div class='wrapper wrapper$count' data-wrapperid='$count' id='wrapper$count'>";
+    foreach ($array as $item) {
+        $number++;
+        ?>
+        <div class="col-md-3">
+            <a href="nummer.php?id=<?php echo $item->id ?>">
+                <div class="options option optionsSmall" href="nummer.php?id=<?php echo $item->id ?>" id="option<?php echo $number ?>" data-text="<?php echo $item->track->name; ?>">
+                    <p id="optionSmall"><?php echo mb_strimwidth($item->name, 0, 15, '...'); ?></p>
+                </div>
+            </a>
+        </div>
+
+        <?php
+        array_push($array1, $item->id);
+    }
+    echo "</div>";
+}
+$_SESSION['songs'] = $array1;
+
+?>
+<div class="backBtn col-md-3">
+    <a href="javascript:history.back()">
+        <div class="options optionBackHome optionsSmall">
+            <span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span>
+            <p id="optionSmall">Terug</p>
+        </div>
+    </a>
+</div>
+<div class="homeBtn col-md-3">
+    <a href="index.php">
+        <div class="options optionBackHome optionsSmall">
+            <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+            <p id="optionSmall">Home</p>
+        </div>
+    </a>
+</div>
+</div>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script type="text/javascript" src="js/bootstrap.js"></script>
 <script type="text/javascript" src="responsivevoice.js"></script>
-<script type="text/javascript" src="action.js"></script>
+<script type="text/javascript">
+    var back = $(".backBtn");
+    var home = $(".homeBtn");
+
+    var wrapper = $(".wrapper");
+    var wrapper3 = $(".wrapper3");
+
+    back.appendTo(wrapper3);
+    home.appendTo(wrapper3);
+</script>
 </body>
 </html>
